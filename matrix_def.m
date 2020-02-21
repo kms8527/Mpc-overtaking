9ece1c2e63a0f02765ee9e4348c9d3d8b9bfc9da
kms8527 <44738230+kms8527@users.noreply.github.com>
2020-02-21 22:33:04 +0900

    Add files via upload

--- matrix_def.m
+++ matrix_def.m
@@ -1,23 +1,30 @@
-function [H_,F_,G_,D_]=matrix_def(v,delt,N)
+function [H_,F_,G_,D_,O_,B1_,B2_]=matrix_def(v,delt,N)
 G = [ ] ; 
 H = 0;
 F = [ ]; 
 D = eye(N) ; 
-A = [ 1 0  ; v*delt 1 ] ; 
-B = [ delt ; v*delt^2/2 ] ; 
-C = [ 0 1 ] ;
- 
+A = [ 1 0 0 ; 0 1 0; v*delt 0 1 ] ; 
+B1 = [ 1; 0 ; 0 ] ;
+B2 = [ 0; v*delt; 0];
+C = [ 0 1 0; 0 0 1 ] ;
+
 Au=[eye(N); -eye(N)]; %2N,N
 Ay=[eye(N); -eye(N)];
+Ax=[eye(N); -eye(N)];
 
 
 G = [ ] ; 
 H = 0;
 F = [ ]; 
 D = eye(N) ; 
-
+O=zeros(length(A));
 % H, G, F, D matrix computation
-for i = 1 : N
+for i = 1 : N-1
+    O = [];
+    for j=1:i
+        O_row=O_row + A^(i-1);
+    end
+    O = [ O ;  O_row];
     G = [ G ; C*A^i ] ;
     F = [ F ; C*A^(i-1)*B ] ; 
     if i ~=1 
@@ -42,3 +49,6 @@ H_=H;
 F_=F;
 G_=G;
 D_=D;
+O_=O;
+B1_=B1;
+B2_=B2;
